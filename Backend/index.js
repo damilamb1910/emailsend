fb1b37b109f137663c93fb276351614359936efe
Damian Lamberti <damilamb1910@gmail.com>
2024-02-23 12:05:24 -0300

    backend url corregido cors

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const cors = require("cors");
 const corsOptions = {
-  origin: "http://localhost:http://localhost:3000",
+  origin: "http://localhost:3000",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
